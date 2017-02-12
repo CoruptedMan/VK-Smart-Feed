8be4f1df23d3600f33241a648b0bbac3974bebce
Alexander Styagov <sstyagov2@gmail.com>
2017-02-12 23:33:20 +0300

    Added Comment.java class. Improved note comments element layout.

--- app/src/main/java/ru/ifmo/practice/model/Comment.java
+++ app/src/main/java/ru/ifmo/practice/model/Comment.java
@@ -1,8 +1,43 @@
 package ru.ifmo.practice.model;
 
-/**
- * Created by sstya on 12.02.2017.
- */
-
 public class Comment {
+    private long id;
+    private long authorId;
+    private long date;
+    private String context;
+    private int likesCount;
+    private boolean userLikes;
+
+    public Comment(long pId, long pAuthorId, long pDate, String pContext, int pLikesCount, boolean pUserLikes) {
+        id = pId;
+        authorId = pAuthorId;
+        date = pDate;
+        context = pContext;
+        likesCount = pLikesCount;
+        userLikes = pUserLikes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getAuthorId() {
+        return authorId;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public boolean isUserLikes() {
+        return userLikes;
+    }
 }
