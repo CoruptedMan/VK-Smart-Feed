fcf824403fb89155088e4f993fdafe8bf63b9300
Alexander Styagov <sstyagov2@gmail.com>
2017-02-19 15:42:31 +0300

    Added animations and onClickListeners to repost dialog buttons

--- app/src/main/java/ru/ifmo/practice/model/dialog/RepostNoteDialogFragment.java
+++ app/src/main/java/ru/ifmo/practice/model/dialog/RepostNoteDialogFragment.java
@@ -5,18 +5,36 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import ru.ifmo.practice.R;
 
 public class RepostNoteDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-
-        builder.setView(inflater.inflate(R.layout.repost_dialog_layout, null));
-
-        return builder.create();
+        final View layout = inflater.inflate(R.layout.repost_dialog_layout, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(layout);
+        AlertDialog dialog = builder.create();
+        final EditText messageEditText = (EditText) layout.findViewById(R.id.repost_dialog_message);
+        TextView repostToWall = (TextView) layout.findViewById(R.id.repost_dialog_my_wall);
+        TextView repostToGroup = (TextView) layout.findViewById(R.id.repost_dialog_group);
+        repostToWall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(messageEditText.getText());
+            }
+        });
+        repostToGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(messageEditText.getText());
+            }
+        });
+        return dialog;
     }
 
 }
